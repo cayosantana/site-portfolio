a7b3ef41f920ee7815dfdb3cac8c789e2bde4d02
cayosantana <cc.santana.js@gmail.com>
2024-03-18 15:46:27 -0300

    Efeito Reveal
    Aplicação da biblioteca Scroll Reveal nas sections do main da pag.

---
+++ reveal.js
@@ -0,0 +1,126 @@
+window.revelar = ScrollReveal({ reset: true })
+
+// Topo do Site
+
+revelar.reveal('.efeito-txt-topo-site', {
+    duration: 2000,
+    distance: '90px'
+})
+
+revelar.reveal('.efeito-img-topo-site', {
+    duration: 2000,
+    distance: '90px',
+    delay: 500
+})
+
+// Titulos do site
+
+revelar.reveal('.titulo', {
+    duration: 2000,
+    distance: '90px'
+})
+
+// Sobre
+
+revelar.reveal('.efeito-container-sobre', {
+    duration: 2000,
+    distance: '90px'
+})
+
+revelar.reveal('.efeito-daley-sobre', {
+    duration: 2000,
+    distance: '90px',
+    delay: 500
+})
+
+// Conhecimentos 
+
+revelar.reveal('.efeito-conhecimentos01', {
+    duration: 2000,
+    distance: '90px',
+    delay: 300
+})
+
+revelar.reveal('.efeito-conhecimentos02', {
+    duration: 2000,
+    distance: '90px',
+    delay: 600
+})
+
+revelar.reveal('.efeito-conhecimentos03', {
+    duration: 2000,
+    distance: '90px',
+    delay: 900
+})
+
+revelar.reveal('.efeito-conhecimentos04', {
+    duration: 2000,
+    distance: '90px',
+    delay: 1200
+})
+
+revelar.reveal('.efeito-conhecimentos05', {
+    duration: 2000,
+    distance: '90px',
+    delay: 1500
+})
+
+revelar.reveal('.efeito-conhecimentos06', {
+    duration: 2000,
+    distance: '90px',
+    delay: 1800
+})
+
+revelar.reveal('.efeito-conhecimentos07', {
+    duration: 2000,
+    distance: '90px',
+    delay: 2100
+})
+
+revelar.reveal('.efeito-conhecimentos08', {
+    duration: 2000,
+    distance: '90px',
+    delay: 2400
+})
+
+// Serviço
+
+revelar.reveal('.efeito-ui', {
+    duration: 2000,
+    distance: '90px',
+    delay: 500
+})
+
+revelar.reveal('.efeito-dev', {
+    duration: 2000,
+    distance: '90px',
+    delay: 1000
+})
+
+// Projetos
+
+revelar.reveal('.efeito-port01', {
+    duration: 2000,
+    distance: '90px',
+    delay: 500
+})
+
+revelar.reveal('.efeito-port02', {
+    duration: 2000,
+    distance: '90px',
+    delay: 1000
+})
+
+revelar.reveal('.efeito-port03', {
+    duration: 2000,
+    distance: '90px',
+    delay: 1500
+})
+
+// Formulario
+
+revelar.reveal('.efeito-form', {
+    duration: 2000,
+    distance: '90px',
+    delay: 500
+})
